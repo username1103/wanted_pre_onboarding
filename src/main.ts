8ef798f12ec576020db94e9cbc6842321fd424c9
myeongil <username1103@naver.com>
2022-06-15 21:16:12 +0900

    feat: CustomLogger 추가

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { WinstonModule } from 'nest-winston';
 import { AppConfigService } from './common/config/app/config.service';
+import { getLogger } from './common/logger/getLogger';
 import { setNestApp } from './setNestApp';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+    logger: WinstonModule.createLogger(getLogger('APP')),
+  });
 
   setNestApp(app);
 
