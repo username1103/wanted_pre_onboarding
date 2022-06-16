4804821fa817b6ec8ee8e198d6541e3042c4be03
myeongil <username1103@naver.com>
2022-06-17 03:32:08 +0900

    feat: swagger server url 수정

--- src/setSwagger.ts
+++ src/setSwagger.ts
@@ -1,6 +1,7 @@
 import { INestApplication } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppConfigService } from './common/config/app/config.service';
+import { ResponseEntity } from './common/response/ResponseEntity';
 import { JobVacancyModule } from './job-vacancy/job-vacancy.module';
 
 export const setSwagger = (app: INestApplication) => {
@@ -9,11 +10,12 @@ export const setSwagger = (app: INestApplication) => {
   const config = new DocumentBuilder()
     .setTitle('Wanted-Pre-Onboarding REST API')
     .setVersion('1.0.0')
-    .addServer(`http://localhost:${appConfigService.port}/v1`)
+    .addServer(`http://localhost:${appConfigService.port}`)
     .build();
 
   const document = SwaggerModule.createDocument(app, config, {
     include: [JobVacancyModule],
+    extraModels: [ResponseEntity],
   });
 
   SwaggerModule.setup('/v1/docs', app, document, {
