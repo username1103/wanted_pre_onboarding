bdcac42e98c3e136561758dbdcf6c41634e0b00c
myeongil <username1103@naver.com>
2022-06-17 21:42:00 +0900

    feat: success response decorator 파라미터 수정

--- src/common/decorator/api-success-response.decorator.ts
+++ src/common/decorator/api-success-response.decorator.ts
@@ -3,7 +3,7 @@ import { ApiProperty, ApiResponse } from '@nestjs/swagger';
 import { ResponseEntity } from '../response/ResponseEntity';
 
 // eslint-disable-next-line @typescript-eslint/ban-types
-export function ApiSuccessResponse(status: number, data: Type<unknown> | Function | [Function] | string) {
+export function ApiSuccessResponse(status: number, data?: Type<unknown> | Function | [Function] | string) {
   class Temp extends ResponseEntity<any> {
     @ApiProperty({ type: data })
     get data() {
