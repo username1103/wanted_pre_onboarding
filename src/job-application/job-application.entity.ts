70114e985704ce90a8d99b2e53ad8fa53db6467e
myeongil <username1103@naver.com>
2022-06-18 02:07:33 +0900

    feat: 지원이력테이블 unique인덱스  추가

--- src/job-application/job-application.entity.ts
+++ src/job-application/job-application.entity.ts
@@ -1,9 +1,10 @@
 import { User } from '../user/user.entity';
-import { Entity, JoinColumn, ManyToOne } from 'typeorm';
+import { Entity, JoinColumn, ManyToOne, Unique } from 'typeorm';
 import { BaseEntity } from '../common/BaseEntity';
 import { JobVacancy } from '../job-vacancy/job-vacancy.entity';
 
 @Entity({ name: 'job_application' })
+@Unique('unique_user_id_job_vacancy_id', ['user', 'jobVacancy'])
 export class JobApplication extends BaseEntity {
   @ManyToOne(() => User, { lazy: true })
   @JoinColumn({ name: 'user_id', referencedColumnName: 'id' })
